6e942c001d3e2fa8a5c0fb7f6a763d2ec3eb2c61
Gia Huy <tovugiahuy@gmail.com>
2025-07-16 16:03:35 +0700

    Cấu hình database

---
+++ server/src/modules/users/schemas/user.schemas.ts
@@ -0,0 +1,54 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema({ timestamps: true })
+export class User {
+  @Prop({ required: true })
+  fullName: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  phone: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ required: true })
+  dateOfBirth: Date;
+
+  @Prop({ required: true })
+  gender: string;
+
+  @Prop({ required: true })
+  address: string;
+
+  @Prop()
+  avatar: string;
+
+  // @Prop({ required: true })
+  // role: string; // e.g., 'patient', 'doctor', 'admin'
+
+  @Prop()
+  is_active: boolean;
+
+  @Prop()
+  codeId: string;
+
+  @Prop()
+  codeExpired: Date;
+
+  @Prop()
+  specialty: string;
+
+  @Prop()
+  licenseNumber: string;
+
+  @Prop({ default: false })
+  agreeTerms: boolean;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
